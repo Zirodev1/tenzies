95202afc2b30e60f4b231f8f40a2331ec7193bc1
Lee Acevedo <91152882+Zirodev1@users.noreply.github.com>
2024-08-02 22:14:25 -0500

    added some stats and css

--- src/App.js
+++ src/App.js
@@ -9,14 +9,41 @@ function App() {
   const [allDices, setAllDices] = useState(allNewDice);
   const [tenzies, setTenzies] = useState(false)
   const [rollCount, setRollCount] = useState(0)
+  const [seconds, setSeconds] = useState(0)
+  const [isActive, setIsActive] = useState(false)
+  const [bestTime, setBestTIme] = useState(() => {
+    const storedBestTime = localStorage.getItem('bestTime');
+    return storedBestTime ? JSON.parse(storedBestTime) : null
+  })
 
   useEffect(() => {
     const allHeld = allDices.every(dice => dice.isHeld)
     const sameValues = allDices.every(dice => dice.value)
 
-    setTenzies(allHeld && sameValues ? true : false )
-    console.log("You won!")
-  },[allDices])
+    if(allHeld && sameValues){
+      setTenzies(true)
+      setIsActive(false)
+      
+      if(bestTime === null || seconds < bestTime){
+        setBestTIme(seconds);
+        localStorage.setItem("bestTime", JSON.stringify(seconds))
+      }
+    }
+  },[allDices, seconds, bestTime])
+
+  useEffect(() => {
+    let interval = null;
+
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds((seconds) => seconds + 1);
+      }, 1000);
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval); 
+  }, [isActive, seconds])
 
   console.log(tenzies)
 
@@ -34,6 +61,10 @@ function App() {
 
   function handleNewGame(){
       setAllDices(allNewDice())
+      setTenzies(false)
+      setRollCount(0)
+      setSeconds(0)
+      setIsActive(false)
   }
 
   function handleRollDice() {
@@ -45,17 +76,22 @@ function App() {
 
   function holdDice(id) {
     setAllDices(prev => prev.map(dice => {
-      return dice.id === id ? {...dice, isHeld: !dice.isHeld} : dice
+      if (dice.id === id) {
+        if (!isActive) setIsActive(true);
+        return { ...dice, isHeld: !dice.isHeld };
+      } else {
+        return dice;
+      }
     }))
   }
 
   return (
     <div className="main">
       {tenzies && <Confetti />}
-      <div>
+      <div className="stats">
         <h3>Rolls: {rollCount}</h3>
-        <h3>Time</h3>
-        <h3>Best Time:</h3>
+        <h3>Time: {seconds}</h3>
+        <h3>Best Time: {bestTime !== null ? `${bestTime}s` : "N/A"}</h3>
       </div>
       <h1 className="title">Tenzies</h1>
       <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
